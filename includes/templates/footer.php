429b6fff54fa4e3b7f3eb9b798cb46b954989da5
Eduardo Escurra <ejescurrat@unac.edu.pe>
2021-07-08 22:02:03 -0500

    Guardando

---
+++ includes/templates/footer.php
@@ -0,0 +1,7 @@
+    <footer class="footer">
+        <p>este es el footer</p>
+    </footer>
+
+    <script src="build/js/bundle.min.js"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,10 @@
+    
+<?php include "includes/templates/header.php"; ?>
+    
+    <main class="contenedor seccion">
+        <h2>Pide tu beca aqui</h2>
+        <h1>Hola Mundo</h1>
+    </main>
+
+<?php include "includes/templates/footer.php"; ?> 
+
