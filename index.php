59f3fc8f8fa0ca3b2d2902e290f3119f77968c65
kiritokun123-xd <david22android@gmail.com>
2021-07-15 21:00:02 -0500

    lecha

--- index.php
+++ index.php
@@ -2,13 +2,8 @@
 <?php include "includes/templates/header.php"; ?>
     
     <main class="contenedor seccion">
-<<<<<<< HEAD
         <h2>Pide tu beca aqui</h2>
         <p>Mira esto prro ya se programar en gith</p>
-=======
-        <h2 class="titulo">Pide tu beca aqui</h2>
-        <h1>Hola Mundo</h1>
->>>>>>> carlos
     </main>
 
 <?php include "includes/templates/footer.php"; ?> 
