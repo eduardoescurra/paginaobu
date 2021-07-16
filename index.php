08eb0d36e74812783e4be2c1e09b755dbc3ace65
Eduardo Escurra <ejescurrat@unac.edu.pe>
2021-07-16 14:45:30 -0500

    guardando

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <main class="contenedor seccion">
         <h2>Pide tu beca aqui</h2>
         <h1 class="titulo">Hola Mundo</h1>
+        <h2>Hola gentita</h2>
     </main>
 
 <?php include "includes/templates/footer.php"; ?> 
