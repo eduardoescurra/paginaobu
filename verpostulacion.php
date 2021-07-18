25a4ef07ec63c2e54aa15ea890658a6745e0daf3
CarlosAndres1810 <carlosoriam18@gmail.com>
2021-07-18 17:57:55 -0500

    Ver postulacion

---
+++ verpostulacion.php
@@ -0,0 +1,29 @@
+    
+<?php include "includes/templates/header.php"; ?>
+    <main class="contenedor base">
+        <div class="principal r-gap">
+            <section class="seccion">
+                <div class="titulo bg-rojo">
+                    <h2>Sin Postulación</h2>
+                </div>
+                <div class="info">
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae officia quae doloremque, sapiente ab ipsam suscipit excepturi id porro quas facilis iusto in, dicta perspiciatis doloribus pariatur inventore illum est.</p>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur molestias quasi optio! Consequuntur expedita saepe libero accusantium non animi molestiae laborum dolore molestias hic ullam iste, sed consequatur maxime fuga.</p>
+                </div>
+            </section>
+
+            <section class="seccion">
+                <div class="titulo bg-azul">
+                    <h2>Ver postulacion</h2>
+                </div>
+                <div class="info">
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae officia quae doloremque, sapiente ab ipsam suscipit excepturi id porro quas facilis iusto in, dicta perspiciatis doloribus pariatur inventore illum est.</p>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur molestias quasi optio! Consequuntur expedita saepe libero accusantium non animi molestiae laborum dolore molestias hic ullam iste, sed consequatur maxime fuga.</p>
+                </div>
+            </section>
+            
+
+        </div>
+    </main>
+<?php include "includes/templates/footer.php"; ?> 
+
