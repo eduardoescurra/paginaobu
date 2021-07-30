d1513510eb91b4f659dc55ec37073e5926c87a31
CarlosAndres1810 <carlosoriam18@gmail.com>
2021-07-29 21:01:12 -0500

    Cambio 2

--- verpostulacion.php
+++ verpostulacion.php
@@ -1,6 +1,6 @@
     
 <?php include "includes/templates/header.php"; ?>
-    <main class="contenedor base">
+    <main class="contenedor baseP">
         <div class="principal r-gap">
             <section class="seccion">
                 <div class="titulo bg-rojo">
@@ -16,10 +16,42 @@
                 <div class="titulo bg-azul">
                     <h2>Ver postulacion</h2>
                 </div>
-                <div class="info verpostulacion">
-                    <div class="tabla">
+                <div class="info tabla">
+                    <div class="columna">
                         <h3>Id</h3>
                         <p>P01</p>
+                    </div>
+                    <div class="columna">
+                        <h3>Facultad</h3>
+                        <p>FIIS</p>
+                    </div>
+                    <div class="columna">
+                        <h3>Escuela</h3>
+                        <p>EPIS</p>
+                    </div>
+                    <div class="columna">
+                        <h3>Calendario</h3>
+                        <p>2021A</p>
+                    </div>
+                    <div class="columna">
+                        <h3>Apellidos</h3>
+                        <p>Lopez Leon</p>
+                    </div>
+                    <div class="columna">
+                        <h3>Nombres</h3>
+                        <p>David Diana</p>
+                    </div>
+                    <div class="columna">
+                        <h3>Fecha</h3>
+                        <p>21/07/21</p>
+                    </div>
+                    <div class="columna">
+                        <h3>Estado</h3>
+                        <p>Recibido</p>
+                    </div>
+                    <div class="columna">
+                        <h3>Anexos</h3>
+                        <p>No</p>
                     </div>
                 </div>
             </section>
