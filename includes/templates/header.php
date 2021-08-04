d22dfdcd6a4be65c58e355292e697ff787b53a4d
kiritokun123-xd <david22android@gmail.com>
2021-08-03 19:40:00 -0500

    verp

--- includes/templates/header.php
+++ includes/templates/header.php
@@ -59,7 +59,7 @@
                     <p class="texto">Postular</p>
                 </a>    
 
-                <a href="#" class="icono">
+                <a href="verpostulacion.php" class="icono">
                     <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-eye" width="20" height="20" viewBox="0 0 24 24" stroke-width="2.5" stroke="#828282" fill="none" stroke-linecap="round" stroke-linejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                     <circle cx="12" cy="12" r="2" />
---
+++ verpostulacion.php
@@ -0,0 +1,62 @@
+    
+<?php include "includes/templates/header.php"; ?>
+    <main class="contenedor baseP">
+        <div class="principal r-gap">
+            <section class="seccion">
+                <div class="titulo bg-rojo">
+                    <h2>Sin Postulación</h2>
+                </div>
+                <div class="info postular">
+                    <p>Parece que aún no realizas tu Postulación, haz click en: </p>
+                    <a href="postular.php">Postular</a>
+                </div>
+            </section>
+
+            <section class="seccion scroll">
+                <div class="titulo bg-azul">
+                    <h2>Ver postulacion Gaaaa</h2>
+                </div>
+                <div class="info tabla">
+                    <div class="columna">
+                        <h3>Id</h3>
+                        <p>P01</p>
+                    </div>
+                    <div class="columna">
+                        <h3>Facultad</h3>
+                        <p>FIIS</p>
+                    </div>
+                    <div class="columna">
+                        <h3>Escuela</h3>
+                        <p>EPIS</p>
+                    </div>
+                    <div class="columna">
+                        <h3>Calendario</h3>
+                        <p>2021A</p>
+                    </div>
+                    <div class="columna">
+                        <h3>Apellidos</h3>
+                        <p>Lopez Leon</p>
+                    </div>
+                    <div class="columna">
+                        <h3>Nombres</h3>
+                        <p>David Diana</p>
+                    </div>
+                    <div class="columna">
+                        <h3>Fecha</h3>
+                        <p>21/07/21</p>
+                    </div>
+                    <div class="columna">
+                        <h3>Estado</h3>
+                        <p>Recibido</p>
+                    </div>
+                    <div class="columna">
+                        <h3>Anexos</h3>
+                        <p>No</p>
+                    </div>
+                </div>
+            </section>
+            
+        </div>
+    </main>
+<?php include "includes/templates/footer.php"; ?> 
+
