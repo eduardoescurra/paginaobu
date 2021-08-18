57320856ac43b99dae4f246c1cd5c59890a6057a
kiritokun123-xd <david22android@gmail.com>
2021-08-18 16:32:39 -0500

    Listo

--- includes/config/database.php
+++ includes/config/database.php
@@ -2,7 +2,7 @@
  
 function conectarDB() : mysqli{
     $db = mysqli_connect('localhost', 'root', 'root', 'obu');
-
+    $db -> set_charset("utf8");
     if(!$db){
         echo "Error no se pudo Conectar";
         exit;
